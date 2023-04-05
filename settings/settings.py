56e6f5d8b15960c48205564122f9674e9bed1df9
shirku <robin.barbulla@gmail.com>
2023-04-05 09:42:46 +0200

    rm throttling

--- settings/settings.py
+++ settings/settings.py
@@ -143,8 +143,6 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "DEFAULT_PARSER_CLASSES": ("rest_framework.parsers.JSONParser",),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny",),
-    "DEFAULT_THROTTLE_CLASSES": ("rest_framework.throttling.AnonRateThrottle",),
-    "DEFAULT_THROTTLE_RATES": {"anon": "1000/day"},
     "PAGE_SIZE": 10,
 }
 
