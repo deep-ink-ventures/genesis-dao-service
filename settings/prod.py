8025ac3f6b644820bc45844aee4e50d3aeba57a2
shirku <robin.barbulla@gmail.com>
2023-04-26 14:28:59 +0200

    update prod envvars

--- settings/prod.py
+++ settings/prod.py
@@ -2,6 +2,7 @@ import os
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 STATICFILES_STORAGE = "core.storage.S3StaticStorage"
+FILE_UPLOAD_CLASS = "core.file_handling.aws.s3_client"
 DEBUG = False
 
 SECRET_KEY = os.environ["SECRET"]
